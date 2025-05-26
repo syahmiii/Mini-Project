468918b11ce7e57c9c455c7a6fea02e1ce232729
syahmiii <syahmisyukri573@gmail.com>
2025-05-26 21:27:09 +0800

    feat: project set up

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Hospital_Management_System.Domain.DatabaseEntities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HospitalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnectionString")));
 
 var app = builder.Build();
 
