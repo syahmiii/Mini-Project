b57a32daf7289514e47594c564ada7e04ed2e663
syahmiii <syahmisyukri573@gmail.com>
2025-06-02 15:15:28 +0800

    feat: add patient

---
+++ Application/Patient/Command/AddPatient.cs
@@ -0,0 +1,39 @@
+﻿namespace Hospital_Management_System.Application.Patient.Command
+{
+    public record AddPatientCommand
+    {
+        public string FirstName { get; init; } = string.Empty;
+        public DateOnly DOB { get; init; }
+        public string ContactNumber { get; init; } = string.Empty;
+        public string Address { get; init; } = string.Empty;
+    }
+
+    public class AddPatientCommandhandler : IRequest<AddPatientCommand>
+    {
+        private readonly HospitalDbContext _context;
+
+        public AddPatientCommandhandler(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(AddPatientCommand request, CancellationToken cancellationToken)
+        {
+
+            Patients patient = new ()
+            {
+                Name = request.FirstName,
+                Dob = request.DOB,
+                ContactNumber = request.ContactNumber,
+                Address = request.Address
+            };
+
+
+            await _context.Patients.AddAsync(patient, cancellationToken: cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+        }
+
+    }
+}
---
+++ Application/Patient/Command/AddPatientDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital_Management_System.Application.Patient.Command
+{
+    public class AddPatientDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateOnly DOB { get; set; }
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+    }
+}
