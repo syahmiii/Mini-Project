1d8ac1a875200d92cc041b5389413e010638da4e
syahmiii <syahmisyukri573@gmail.com>
2025-06-02 15:15:47 +0800

    feat: get patients

---
+++ Application/Patient/Query/GetPatients/GetPatients.cs
@@ -0,0 +1,30 @@
+﻿namespace Hospital_Management_System.Application.Patient.Query.GetPatients
+{
+    public record GetPatientsQuery : IRequest<List<GetPatientsDto>>
+    {
+        public List<int> PatientId { get; init; } = new();
+    }
+
+    public class GetPatientsQueryHandler : IRequestHandler<GetPatientsQuery, List<GetPatientsDto>>
+    {
+        private readonly HospitalDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetPatientsQueryHandler(HospitalDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetPatientsDto>> Handle(GetPatientsQuery request, CancellationToken cancellationToken)
+        {
+            List<GetPatientsDto> patients = await _dbContext.Patients
+                .AsNoTracking()
+                .Where(p => request.PatientId.Contains(p.Id))
+                .ProjectTo<GetPatientsDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return patients;
+        }
+    }
+}
---
+++ Application/Patient/Query/GetPatients/GetPatientsDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Hospital_Management_System.Application.Patient.Query.GetPatients
+{
+    public class GetPatientsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
+    }
+
+    public class GetPatientsDtoProfile : Profile
+    {
+        public GetPatientsDtoProfile()
+        {
+            CreateMap<Patients, GetPatientsDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.Dob ?? DateOnly.MinValue))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address));
+        }
+    }
+}
