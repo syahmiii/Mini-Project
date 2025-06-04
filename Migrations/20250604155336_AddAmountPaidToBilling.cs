52ece3d9c3e3d5d4f47cac68335e1f671ffbb26c
syahmiii <syahmisyukri573@gmail.com>
2025-06-04 23:57:49 +0800

    feat: efcore migration

---
+++ Migrations/20250604155336_AddAmountPaidToBilling.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmountPaidToBilling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PaidAmount",
+                table: "Billings",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaidAmount",
+                table: "Billings");
+        }
+    }
+}
