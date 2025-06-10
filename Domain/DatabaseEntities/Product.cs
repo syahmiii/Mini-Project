22838e8333a9f665461922d12b86dcf3e36b3f1d
syahmiii <syahmisyukri573@gmail.com>
2025-06-10 14:26:00 +0800

    feat: create product table

---
+++ Domain/DatabaseEntities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital_Management_System.Domain.DatabaseEntities
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal Price { get; set; } = 0.0m;
+        public int StockQuantity { get; set; } = 0;
+    }
+}
