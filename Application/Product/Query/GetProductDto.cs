a114c9e629c35d971ce4992f4a83c69fd52d6120
syahmiii <syahmisyukri573@gmail.com>
2025-06-10 14:26:30 +0800

    sell and add product

---
+++ Application/Product/Query/GetProductDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Hospital_Management_System.Application.Product.Query
+{
+    public class GetProductDto
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal Price { get; set; } = 0.0m;
+        public int StockQuantity { get; set; } = 0;
+
+    }
+
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Domain.DatabaseEntities.Product, GetProductDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.ProductName))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+                .ForMember(dest => dest.StockQuantity, opt => opt.MapFrom(src => src.StockQuantity));
+        }
+    }
+}
