902d495c84225fb46884abcc0aae2ef1c97f1869
syahmiii <syahmisyukri573@gmail.com>
2025-07-08 18:27:18 +0800

    feat: implement controller

---
+++ Controllers/Appointment.cs
@@ -0,0 +1,37 @@
+﻿using Hospital_Management_System.Application.Appointment.Command;
+using Hospital_Management_System.Application.Appointment.Query.GetAppointment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_Management_System.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AppointmentController(IMediator mediator)
+        {
+
+            _mediator = mediator;
+        }
+
+        [HttpGet(nameof(GetAppointment))]
+        public Task<GetAppointmentQueryDto> GetAppointment([FromQuery] GetAppointmentQuery request)
+        {
+            Task<GetAppointmentQueryDto> result = _mediator.Send(request);
+
+            return result;
+        }
+
+        [HttpPost(nameof(AddAppointment))]
+        public async Task<IActionResult> AddAppointment([FromBody]AddAppointmentCommand command)
+        {
+            await _mediator.Send(command);
+
+            return NoContent();
+        
+        }
+
+    }
+}
